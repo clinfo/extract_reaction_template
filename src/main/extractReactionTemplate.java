bfbe565d564561e809adebc30cf12f90618c0272
sishida21 <ishida.shouichi.57a@st.kyoto-u.ac.jp>
2019-12-16 23:14:59 +0900

    refactor: organize members in extractReactionTemplate.java

--- src/main/extractReactionTemplate.java
+++ src/main/extractReactionTemplate.java
@@ -4,15 +4,10 @@ import chemaxon.formats.MolExporter;
 import chemaxon.formats.MolFormatException;
 import chemaxon.formats.MolImporter;
 import chemaxon.standardizer.Standardizer;
-import chemaxon.struc.MolAtom;
 import chemaxon.struc.Molecule;
 import chemaxon.struc.RxnMolecule;
-import chemaxon.util.iterator.IteratorFactory;
-import chemaxon.util.iterator.IteratorFactory.AtomIterator;
-import chemaxon.util.iterator.IteratorFactory.AtomNeighbourIterator;
 import com.chemaxon.mapper.AutoMapper;
 import com.chemaxon.mapper.Mapper;
-import org.apache.commons.lang3.BooleanUtils;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
@@ -111,8 +106,8 @@ public class extractReactionTemplate {
                     mapper.map(reaction);
                     RxnMolecule reactionCloneForCore = reaction.clone();
                     RxnMolecule reactionCloneFor1Neighbor = reaction.clone();
-                    getReactionTemplate(reactionCloneForCore, "0");
-                    getReactionTemplate(reactionCloneFor1Neighbor, "1");
+                    Core.getReactionTemplate(reactionCloneForCore, "0");
+                    Core.getReactionTemplate(reactionCloneFor1Neighbor, "1");
                     if (reactionCloneForCore.getProduct(0).isEmpty() | reactionCloneFor1Neighbor.getProduct(0).isEmpty()) {
                         continue;
                     }
@@ -136,87 +131,5 @@ public class extractReactionTemplate {
         }
     }
 
-    private static void getReactionTemplate(RxnMolecule reaction, String neighborNum) throws IllegalArgumentException {
-        // Process reactant(s) included in a reaction
-        for (int i = 0; i < reaction.getReactantCount(); i++) {
-            Molecule reactant = reaction.getReactant(i);
-            AtomIterator iterator = setPropertyWithSpecificCondition(reactant, neighborNum);
-            removeAtomWithNoProperty(iterator);
-        }
-        // Process product(s) included in a reaction
-        for (int i = 0; i < reaction.getProductCount(); ++i) {
-            Molecule product = reaction.getProduct(i);
-            AtomIterator iterator = setPropertyWithSpecificCondition(product, neighborNum);
-            removeAtomWithNoProperty(iterator);
-        }
-    }
-
-    private static void removeAtomWithNoProperty(AtomIterator iterator) {
-        while (iterator.hasNext()) {
-            MolAtom atom = iterator.next();
-            Boolean reserveAtom = (Boolean) atom.getProperty("reserve");
-            Boolean reserveNAtom = (Boolean) atom.getProperty("nReserve");
-            if (BooleanUtils.isNotTrue(reserveAtom) & BooleanUtils.isNotTrue(reserveNAtom)) {
-                iterator.remove();
-            }
-        }
-    }
-
-    private static AtomIterator setPropertyWithSpecificCondition(Molecule mol, String neighborNum) {
-        AtomIterator iterator;
-        switch (neighborNum) {
-            case "0": {
-                iterator = mol.getAtomIterator();
-                setPropertyOnMappedAtom(iterator);
-                return mol.getAtomIterator();
-            }
-            case "1": {
-                iterator = mol.getAtomIterator();
-                setPropertyOnMappedAtom(iterator);
-                iterator = mol.getAtomIterator();
-                setPropertyOnNeighborAtom(iterator, mol);
-                return mol.getAtomIterator();
-            }
-            default: {
-                System.out.println("[ERROR] check the neighbor atom number is valid");
-                System.exit(1);
-                return null;  // Avoid compile errors
-            }
-        }
-    }
-
-    private static void setPropertyOnMappedAtom(AtomIterator iterator) {
-        while (iterator.hasNext()) {
-            MolAtom atom = iterator.next();
-            int mapNum = atom.getAtomMap();
-            if (mapNum != 0) {
-                atom.putProperty("reserve", true);
-            }
-        }
-    }
-
-    private static void setPropertyOnNeighborAtom(AtomIterator iterator, Molecule reaction) {
-        Boolean reserveAtom;
-        Boolean reserveNAtom;
-        while (iterator.hasNext()) {
-            MolAtom atom = iterator.next();
-            reserveAtom = (Boolean) atom.getProperty("reserve");
-            // For only processing reaction center atoms which atom-mapped by AutoMapper.
-            if (BooleanUtils.isTrue(reserveAtom)) {
-                // set "reserve" to the reaction center atoms and "nReserve" to the first neighbor atoms.
-                AtomNeighbourIterator nIterator = new IteratorFactory(reaction).createAtomNeighbourIterator(atom);
-                while (nIterator.hasNext()) {
-                    MolAtom nAtom = nIterator.next();
-                    reserveAtom = (Boolean) nAtom.getProperty("reserve");
-                    reserveNAtom = (Boolean) nAtom.getProperty("nReserve");
-                    // for preventing overwrite on reaction center atoms / first neighbor atoms
-                    if (BooleanUtils.isTrue(reserveAtom) & BooleanUtils.isTrue(reserveNAtom)) {
-                        continue;
-                    }
-                    nAtom.putProperty("nReserve", true);
-                }
-            }
-        }
-    }
 }
 
