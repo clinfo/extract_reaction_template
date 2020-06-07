55f4a61302c3e154565abe49f8a14ff0521c3a90
sishida21 <ishida.shouichi.57a@st.kyoto-u.ac.jp>
2020-06-07 20:11:08 +0900

    refactor: change delimiters from comma to tab character.

--- src/main/ExtractReactionTemplateFromReaxysData.java
+++ src/main/ExtractReactionTemplateFromReaxysData.java
@@ -210,11 +210,11 @@ public class ExtractReactionTemplateFromReaxysData {
                 if (!checkAndFixValenceProperty(reactionCore) | !checkAndFixValenceProperty(reaction1Neighbor)) {
                     continue;
                 }
-                writer.write(idList.get(i) + "," +
-                        MolExporter.exportToFormat(product, FORMAT) + "," +
-                        MolExporter.exportToFormat(sortReactantsInReaction(reaction), FORMAT) + "," +
-                        MolExporter.exportToFormat(sortReactantsInReaction(reactionCore), FORMAT) + "," +
-                        MolExporter.exportToFormat(sortReactantsInReaction(reaction1Neighbor), FORMAT) + "," +
+                writer.write(idList.get(i) + "\t" +
+                        MolExporter.exportToFormat(product, FORMAT) + "\t" +
+                        MolExporter.exportToFormat(sortReactantsInReaction(reaction), FORMAT) + "\t" +
+                        MolExporter.exportToFormat(sortReactantsInReaction(reactionCore), FORMAT) + "\t" +
+                        MolExporter.exportToFormat(sortReactantsInReaction(reaction1Neighbor), FORMAT) + "\t" +
                         max_pub_year);
                 writer.newLine();
             }
